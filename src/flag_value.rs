57cfceeb3ad8e5b752e94fc899500ebc507f73a4
Masaki Hara <ackie.h.gmai@gmail.com>
2019-09-15 14:39:24 +0900

    Add some tests.

--- src/flag_value.rs
+++ src/flag_value.rs
@@ -77,3 +77,47 @@ impl FlagValue for bool {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_bool() -> Result<(), FlagParseError> {
+        for &default in &[false, true] {
+            let flag = default;
+            assert_eq!(flag.is_bool_flag(), true);
+
+            let mut flag = default;
+            flag.set(None, None)?;
+            assert_eq!(flag, true);
+
+            for &s in &["0", "f", "F", "false", "FALSE", "False"] {
+                let mut flag = default;
+                flag.set(Some(OsStr::new(s)), None)?;
+                assert_eq!(flag, false);
+            }
+
+            for &s in &["1", "t", "T", "true", "TRUE", "True"] {
+                let mut flag = default;
+                flag.set(Some(OsStr::new(s)), None)?;
+                assert_eq!(flag, true);
+            }
+
+            for &s in &["", "00", "2", "fALSE", "tRUE", "no", "yes", "off", "on"] {
+                let mut flag = default;
+                assert!(flag.set(Some(OsStr::new(s)), None).is_err());
+            }
+
+            #[cfg(any(unix, target_os = "redox"))]
+            for &s in &[b"true\xA0" as &[u8], b"\xE3"] {
+                use std::os::unix::ffi::OsStrExt;
+
+                let mut flag = default;
+                assert!(flag.set(Some(OsStr::from_bytes(s)), None).is_err());
+            }
+        }
+
+        Ok(())
+    }
+}
